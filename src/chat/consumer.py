892e5f44f65a8e48a606b216d429f2d7461dfaf9
ujjuboi <dev@LAPTOP-HC8BJ4AO.localdomain>
2021-03-01 19:51:59 +0530

    Added basic websocket operations
    Basic understanding of websocket and asynchronous comms

--- src/chat/consumer.py
+++ src/chat/consumer.py
@@ -5,3 +5,13 @@ from channels.consumer import AsyncConsumer
 from channels.db import database_sync_to_async
 
 from .models import Thread, ChatMessage
+
+class ChatConsumer(AsyncConsumer):
+  async def websocket_connect(self, event): #async keyword makes the function communicate in asynchronous mode
+    print("connected", event)
+
+  async def websocket_recieve(self, event): 
+    print("recieve", event)
+
+  async def websocket_disconnect(self, event):
+    print("disconnected", event)    
