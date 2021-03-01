496af3fa24494e6173da1c515ede1db83c8e5e64
ujjuboi <dev@LAPTOP-HC8BJ4AO.localdomain>
2021-03-01 19:38:59 +0530

    Configured for ASGI deployment
    Added consumer.py (similar to views for http); all main configurations in routing.py (important)

---
+++ src/chat/consumer.py
@@ -0,0 +1,7 @@
+import asyncio
+import json
+from django.contrib.auth import get_user_model
+from channels.consumer import AsyncConsumer
+from channels.db import database_sync_to_async
+
+from .models import Thread, ChatMessage
