a252a7a82dd8e191e9191c0df448dfe869c50a8f
ujjuboi <dev@LAPTOP-HC8BJ4AO.localdomain>
2021-03-01 20:30:30 +0530

    Established routing

--- src/cfehome/routing.py
+++ src/cfehome/routing.py
@@ -1,5 +1,18 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 
+from chat.consumer import ChatConsumer
 application = ProtocolTypeRouter({
-  
+  #Can also open websocket and http at the same time and make them work together (Handled by channels)
+  'websocket': AllowedHostsOriginValidator(
+    AuthMiddlewareStack(
+      URLRouter(
+        [
+          url(r"^(?P<username>[\w.@+-]+)", ChatConsumer), #don't do it .as_view(), simple export)
+        ]
+      )
+    ) #Do you want the requested user to be inside the websocket and access that user?
+  ) #Directs any URL to the allowed hosts in settings.py
 })
